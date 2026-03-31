5c0cca49e4be7e384213a5a04cb3fa675be45c5b
Emmanuel <manu.scharnoski@gmail.com>
2026-03-31 10:08:18 -0300

    feat: adiciona card de imoveis para paginas diferentes

---
+++ src/components/ImovelCard.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { formatarFinalidade, formatarPreco } from "../lib/formatar";
+import type { Imovel } from "../lib/types";
+
+interface ImovelCardProps {
+  imovel: Imovel;
+}
+
+export default function ImovelCard({ imovel }: ImovelCardProps) {
+  return (
+    <article className="cardImovel">
+      <span className="propriedadeTag">
+        {formatarFinalidade(imovel.finalidade)}
+      </span>
+
+      <Link href={`/imoveis/${imovel.codigo}`}>
+        <img src={imovel.imagem} alt={imovel.titulo} className="imagemImovel" />
+      </Link>
+
+      <div className="cardGrid">
+        <div className="info">
+          <span>{imovel.categoria}</span>
+          <span>Cod. {imovel.codigo}</span>
+        </div>
+
+        <div className="nomeCard">
+          <div>
+            <Link href={`/imoveis/${imovel.codigo}`}>{imovel.titulo}</Link>
+            <p>
+              {imovel.cidade} - {imovel.bairro}
+            </p>
+          </div>
+          <span>
+            <i className="fa-solid fa-ruler-combined" /> {imovel.area} m2
+          </span>
+        </div>
+
+        <div className="atributos">
+          <span>
+            <i className="fa-solid fa-bed" /> {imovel.quartos}
+          </span>
+          <span>
+            <i className="fa-solid fa-bath" /> {imovel.banheiros}
+          </span>
+          <span>
+            <i className="fa-solid fa-car" /> {imovel.vagas}
+          </span>
+        </div>
+
+        <div className="preco">
+          <p>Preco</p>
+          <strong>
+            R$ {formatarPreco(imovel.preco)}
+            {imovel.finalidade === "locacao" ? "/mes" : ""}
+          </strong>
+        </div>
+
+        <Link href={`/imoveis/${imovel.codigo}`} className="verDetalhes">
+          Ver detalhes
+        </Link>
+      </div>
+    </article>
+  );
+}
