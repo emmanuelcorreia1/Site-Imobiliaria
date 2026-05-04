604fb875a7c5e0e9b0301965b36ae7b3489dc95f
EmmanuelForcetech <emmanuel.correia@forcetechjr.com.br>
2026-05-04 16:35:41 -0300

    fix index

--- pages/imoveis/index.tsx
+++ pages/imoveis/index.tsx
@@ -1,17 +1,22 @@
 import type { GetServerSideProps } from "next";
 import ListagemPage from "../../src/components/ListagemPaginas";
 import type { ListagemPageProps } from "../../src/components/ListagemPaginas";
-import { filtrarImoveis, obterOpcoesCatalogo } from "../../src/lib/imovelService";
+import { filtrarImoveis, obterOpcoesCatalogo } from "../../src/lib/imovelRepository";
 
 export default function CatalogoPage(props: ListagemPageProps) {
   return <ListagemPage {...props} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const [busca, catalogo] = await Promise.all([
+    filtrarImoveis(query),
+    obterOpcoesCatalogo(),
+  ]);
+
   return {
     props: {
-      busca: filtrarImoveis(query),
-      catalogo: obterOpcoesCatalogo(),
+      busca,
+      catalogo,
       consulta: query,
       cabecalho: "Catalogo completo",
       pagina: "imoveis",
