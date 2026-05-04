1cf9c33bfc4fd546a122cf173802f3c822d5cf3a
EmmanuelForcetech <emmanuel.correia@forcetechjr.com.br>
2026-05-04 16:34:47 -0300

    fix

--- pages/api/imoveis.ts
+++ pages/api/imoveis.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { filtrarImoveis } from "../../src/lib/imovelService";
+import { filtrarImoveis } from "../../src/lib/imovelRepository";
 import type { BuscaImoveis } from "../../src/lib/types";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<BuscaImoveis & { total: number }>,
 ) {
-  const busca = filtrarImoveis(req.query);
+  const busca = await filtrarImoveis(req.query);
 
   res.status(200).json({
     total: busca.itens.length,
